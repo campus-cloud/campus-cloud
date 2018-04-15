64d6ec8b55d49049cac05dd2fc0c71dd27897b27
Ethan <epchow@hawaii.edu>
2018-04-14 14:32:39 -1000

    Improved the 404 Not Found page

--- app/imports/ui/pages/NotFound.jsx
+++ app/imports/ui/pages/NotFound.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 /** Render a Not Found page if the user enters a URL that doesn't match any route. */
 class NotFound extends React.Component {
   render() {
+    const errorBoxStyle = {
+      padding: '10px 0 20px',
+      background: '#E3F2FD',
+      textAlign: 'center',
+    };
+
     return (
-      <Header as="h2" textAlign="center">
-        <p>Page not found</p>
-      </Header>
+        <Container fluid style={{ padding: '0 0 50px', color: '#444', textAlign: 'center', fontFamily: '"Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif', lineHeight: 'normal' }}>
+          <Container fluid style={errorBoxStyle}>
+            <Container fluid style={{ margin: '20px', fontSize: '72px', color: '#2196F3' }}>We couldn’t find that!</Container>
+            <Container fluid style={{ marginBottom: '30px', fontSize: '16px', color: '#64B5F6' }}>404 Not Found</Container>
+          </Container>
+          <Container fluid style={{ marginTop: '50px' }}>Sorry, that page or resource does not exist!</Container>
+          <Container fluid style={{ marginTop: '25px' }}>Try the <Link to="/">home page</Link>.</Container>
+        </Container>
     );
   }
 }
