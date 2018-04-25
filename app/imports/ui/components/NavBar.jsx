d97a3dd9f98ab677b64f9b927ffecd942a440293
Ethan <epchow@hawaii.edu>
2018-04-25 00:11:24 -1000

    Updated navigation bar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Header } from 'semantic-ui-react';
+import { Menu, Header, Icon, Dropdown } from 'semantic-ui-react';
+import { Roles } from 'meteor/alanning:roles';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
@@ -14,23 +15,24 @@ class NavBar extends React.Component {
           <Header inverted as='h1'>Campus Cloud</Header>
         </Menu.Item>
         <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>Browse RIOs</Menu.Item>
-        <Menu.Item position="right">
-          {this.props.currentUser === '' ? (
-            <Dropdown text="Login" pointing="top right" icon={'user'}>
+        {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+            <Menu.Item as={NavLink} activeClassName="active" exact to="/import" key='import'>Import RIOs</Menu.Item>
+        ) : ''}
+        {this.props.currentUser === '' ? (
+          <Menu.Item as={NavLink} position="right" activeClassName="active" exact to="/signin" key='signin'>
+            Login <Icon name="user" style={{ margin: '0 0 0 0.35714286em' }}/>
+          </Menu.Item>
+        ) : (
+          <Menu.Item position="right">
+            <Dropdown text={this.props.currentUser} pointing="top right"
+                      icon={<Icon name="user" style={{ margin: '0 0 0 0.35714286em' }}/>}>
               <Dropdown.Menu>
-                <Dropdown.Item icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
-                <Dropdown.Item icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
+                <Dropdown.Item icon="user" text="User Page" as={NavLink} exact to="/user"/>
+                <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
               </Dropdown.Menu>
             </Dropdown>
-          ) : (
-            <Dropdown text={this.props.currentUser} pointing="top right" icon={'user'}>
-              <Dropdown.Menu>
-                <Dropdown.Item icon="user" text="User Page" as={NavLink} exact to="/userpage"/>
-               <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
-              </Dropdown.Menu>
-            </Dropdown>
-          )}
-        </Menu.Item>
+          </Menu.Item>
+        )}
       </Menu>
     );
   }
